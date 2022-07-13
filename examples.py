24bdc2e5416c1eaba5d294a04cf9ffcfc5d144c3
DhirenShivdasani <51030977+DhirenShivdasani@users.noreply.github.com>
2022-07-13 22:20:53 +0200

    Update examples.py

--- examples.py
+++ examples.py
@@ -13,17 +13,16 @@ from sklearn.manifold import TSNE
 
 def example1():
 	example_img = load_img('Test/genericface2.png')
-	st.image(example_img)
+	st.image(cv2.resize(example_img, (300,300)))
 
 	test = st.button('Find celebrity lookalike')
 	all_embeds = pd.read_csv('img_embeddings134.csv', index_col = [0])
-	df = make_distances_df('Test/genericface2.png', all_embeds)
+	example_img_embedding = path_to_embedding('Test/genericface2.png')
+	df = make_distances_df(example_img_embedding, all_embeds)
 
 	if test:
 		mtcnn_img = img_to_mtcnn(example_img)
-		# st.image(load_mtcnn(mtcnn_img), clamp = True)
 		celeb_look_alike_img = get_celeb_look_alike_img(df, all_embeds)
-		# st.image([cv2.resize(celeb_look_alike_img, (200,200)), cv2.resize(load_mtcnn(mtcnn_img), (200,200))], clamp = True)
 		with st.spinner('Identifying celebrity lookalike'):
 		    time.sleep(2)
 		st.success('Done!')
@@ -33,17 +32,16 @@ def example1():
 
 def example2():
 	example_img = load_img('Test/woman2.jpeg')
-	st.image(example_img)
+	st.image(cv2.resize(example_img, (300,300)))
 
 	test = st.button('Find celebrity lookalike')
 	all_embeds = pd.read_csv('img_embeddings134.csv', index_col = [0])
-	df = make_distances_df('Test/woman2.jpeg', all_embeds)
+	example_img_embedding = path_to_embedding('Test/woman2.jpeg')
+	df = make_distances_df(example_img_embedding, all_embeds)
 
 	if test:
 		mtcnn_img = img_to_mtcnn(example_img)
-		# st.image(load_mtcnn(mtcnn_img), clamp = True)
 		celeb_look_alike_img = get_celeb_look_alike_img(df, all_embeds)
-		# st.image([cv2.resize(celeb_look_alike_img, (200,200)), cv2.resize(load_mtcnn(mtcnn_img), (200,200))], clamp = True)
 		with st.spinner('Identifying celebrity lookalike'):
 		    time.sleep(2)
 		st.success('Done!')
